e748f19b7316d1a0792cb20744768bca9423a3eb
Yoann Gourves <yoann.gourves@isima.fr>
2024-09-27 22:58:23 +0200

    init

---
+++ app.py
@@ -0,0 +1,56 @@
+#use flask to create a web server consisting of a login page and an endpoint providing a webpage
+
+from flask import Flask, render_template, request, redirect, url_for, session
+
+from fileReader import addOpinion, getOpinions, getSubjects, getValuesForOpinions, isUserAuthorized
+
+app = Flask(__name__)
+
+app.secret_key='ultra_secret_key_used_for_session_but_i_dont_care_so_this_shitty_string_will_be_used'
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    session['username'] = request.form['username']
+    return redirect(url_for('poll', index=0))
+
+@app.route('/poll/<int:index>')
+def poll(index):
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if not isUserAuthorized():
+        return "You are not authorized to view this page"
+    if index is None:
+        index = 0
+    subject = getSubjects(index)
+    return render_template('poll.html', subject=subject, opinions=getOpinions(subject), index=index, values=getValuesForOpinions(subject, session['username']), zip=zip)
+
+@app.route('/poll/<int:index>', methods=['POST'])
+def submit_poll(index):
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if not isUserAuthorized():
+        return "You are not authorized to view this page"
+    subject = getSubjects(index).strip()
+    mapValueOpinion = {}
+    for opinion in getOpinions(subject):
+        if opinion in request.form:
+            mapValueOpinion[opinion] = request.form[opinion]
+
+    for opinion, value in mapValueOpinion.items():
+        addOpinion(subject, opinion, value, session['username'])
+    return redirect(url_for('poll', index=index+1))
+
+@app.route('/poll/addOpinion/<int:index>', methods=['POST'])
+def add_opinion(index):
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    if not isUserAuthorized():
+        return "You are not authorized to view this page"
+    subject = getSubjects(index)
+    opinion = request.form['new_opinion']
+    addOpinion(subject, opinion, -1, session['username'])
+    return redirect(url_for('poll', index=index))
---
+++ fileReader.py
@@ -0,0 +1,121 @@
+from os import fdopen, remove
+import os
+from shutil import copymode, move
+from tempfile import mkstemp
+from flask import session
+
+
+def isUserAuthorized():
+    # Check if the user is authorized by checking if the session username is contained in the multiline file authorizedUser.txt
+    with open('authorizedUser.txt', 'r') as file:
+        authorizedUsers = file.readlines()
+        for user in authorizedUsers:
+            if session['username'] in user:
+                return True
+    return False
+
+def getNumberOfUsers():
+    # Get the number of users from the file authorizedUser.txt
+    with open('authorizedUser.txt', 'r') as file:
+        authorizedUsers = file.readlines()
+        return len(authorizedUsers)
+    
+def getUserIndex(username):
+    # Get the index of the user from the file authorizedUser.txt
+    with open('authorizedUser.txt', 'r') as file:
+        authorizedUsers = file.readlines()
+        for i in range(len(authorizedUsers)):
+            if username in authorizedUsers[i]:
+                return i
+    return -1
+
+def getSubjects(index):
+    # Get the subjects from the file subjects.txt
+    with open('subjects.txt', 'r') as file:
+        subjects = file.readlines()
+        if index is None:
+            return subjects
+        if index < 0 or index >= len(subjects):
+            return "Invalid index"
+        return subjects[index].strip()
+    
+def getOpinions(subject):
+    # Get the opinions from the file opinions_{subject}.txt
+    opinions = []
+    # verify if file exist create it if not
+    if not os.path.exists(f'opinions_{subject.strip()}.txt'):
+        with open(f'opinions_{subject.strip()}.txt', 'w') as file:
+            file.write('')
+            
+    with open(f'opinions_{subject.strip()}.txt', 'r') as file:
+        opinions = file.readlines()
+    
+    for i in range(len(opinions)):
+        opinions[i] = opinions[i].strip()
+
+    return opinions
+
+def addOpinion(subject, opinion, value, username):
+    # add opinion if not already present then add the value to the opinion_{subject}_values.txt file
+    opinions = getOpinions(subject)
+    if opinion not in opinions:
+        opinions.append(opinion)
+        with open(f'opinions_{subject.strip()}.txt', 'w') as file:
+            # split the opinion by newline
+            file.writelines(opinion.strip() + '\n' for opinion in opinions)
+    lineToAppend=opinions.index(opinion)
+    userIndex=getUserIndex(username)
+    # verify if file exist create it if not
+    if not os.path.exists(f'opinions_{subject.strip()}_values.txt'):
+        with open(f'opinions_{subject.strip()}_values.txt', 'w') as file:
+            newLine='-1,'*getNumberOfUsers()
+            newLine=newLine[:-1]+'\n'
+            file.write(newLine)
+    with open(f'opinions_{subject.strip()}_values.txt', 'r') as file:
+        fh, abs_path = mkstemp()
+        with fdopen(fh,'w') as new_file:
+            done=False
+            for i,line in enumerate(file):
+                # if empty line write '-1,' for each user
+                if line.strip()=='':
+                    newLine='-1,'*getNumberOfUsers()
+                    newLine=newLine[:-1]+'\n'
+                    new_file.write(newLine)
+                elif i==lineToAppend:
+                    newLine=line
+                    values=newLine.split(',')
+                    values[userIndex]=str(value)
+                    newLine=','.join(values)
+                    new_file.write(newLine)
+                    done=True
+                else:
+                    new_file.write(line)
+            if not done:
+                newLine='-1,'*getNumberOfUsers()
+                newLine=newLine[:-1]+'\n'
+                new_file.write(newLine)
+                
+    file_path=f'opinions_{subject.strip()}_values.txt'
+    #Copy the file permissions from the old file to the new file
+    copymode(file_path, abs_path)
+    #Remove original file
+    remove(file_path)
+    #Move new file
+    move(abs_path, file_path)
+            
+
+def getValuesForOpinions(subject,username):
+    # get the values for the opinions from the file opinions_{subject}_values.txt
+    userIndex=getUserIndex(username)
+    values=[]
+    # verify if file exist create it if not
+    if not os.path.exists(f'opinions_{subject.strip()}_values.txt'):
+        values=['']*len(getOpinions(subject))
+        return values
+    with open(f'opinions_{subject.strip()}_values.txt', 'r') as file:
+        for line in file:
+            vote=line.strip().split(',')
+            if len(vote)>userIndex:
+                values.append(vote[userIndex] if vote[userIndex]!='-1' else '')
+
+    return values
